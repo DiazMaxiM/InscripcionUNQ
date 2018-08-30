eedc18fb42068df7c182d212c76153f219394cc3
rozaracho <zaracho.rosali@gmail.com>
2018-08-29 21:50:19 -0300

    Starting to create component and service to show the student's poll screen

--- src/main/webapp/inscripcionUNQ-app/src/app/auth/auth.service.ts
+++ src/main/webapp/inscripcionUNQ-app/src/app/auth/auth.service.ts
@@ -2,13 +2,14 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Http, Response } from '@angular/http';
+import {Poll} from '../student-poll/poll.model';
 
 @Injectable()
 export class AuthService {
   constructor(private http: Http) {}
 
   login(dni: String) {
-    return this.http.get('/api/user/' + dni)
+    return this.http.get('/api/poll/user/' + dni)
     .pipe(
         map((res: Response) => res.json())
     );
--- src/main/webapp/inscripcionUNQ-app/src/app/auth/signin-screen.component.ts
+++ src/main/webapp/inscripcionUNQ-app/src/app/auth/signin-screen.component.ts
@@ -1,6 +1,9 @@
 import { Component} from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
+import { PollService } from '../student-poll/poll.service';
+import {Router} from '@angular/router';
+import {Poll} from '../student-poll/poll.model';
 
 @Component({
   selector: 'app-signin-screen',
@@ -8,14 +11,23 @@ import { AuthService } from './auth.service';
 })
 export class SigninScreenComponent {
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private pollService: PollService
+  ) {}
 
   dniFormControl = new FormControl('', [
       Validators.required
     ]);
   onSubmit() {
     if (this.dniFormControl.valid) {
-      this.authService.login(this.dniFormControl.value).subscribe(data => {console.log(data)},
+      this.authService.login(this.dniFormControl.value).subscribe(polls => {
+      console.log(polls);
+      this.pollService.sendPolls(polls);
+      },
           err => {console.log('error')});
-    }}
+    }
+      this.router.navigate(['encuestas']);
+  }
 }
