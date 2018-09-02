d6032cafc071d836afb2d855cd4386e2622a8e5e
Ingrid Calderon <ingridgcalderon@gmail.com>
2018-09-01 21:16:59 -0300

    Create page of selection of subjects (with hardcoded data)

---
+++ src/main/webapp/inscripcionUNQ-app/src/app/select-subjects/selectsubjects-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-selectsubjects-screen',
+  templateUrl: './selectsubjects-screen.component.html',
+  styleUrls: ['./selectsubjects-screen.component.css']
+})
+
+export class SelectSubjectsComponent {
+
+}
