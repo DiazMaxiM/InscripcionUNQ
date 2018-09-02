74d718e2ea9312e19efce37d435ec555d8a7fae2
Ingrid Calderon <ingridgcalderon@gmail.com>
2018-09-01 21:20:41 -0300

    Add routing and module of selected subjects

--- src/main/webapp/inscripcionUNQ-app/src/app/app.routing.ts
+++ src/main/webapp/inscripcionUNQ-app/src/app/app.routing.ts
@@ -3,6 +3,7 @@ import {SigninScreenComponent} from './auth/signin-screen.component';
 import {PollScreenComponent} from './poll/poll-screen.component';
 import { DataVerificationComponent } from './data-verification/data-verification-screen.component';
 import { SubjectScreenComponent} from './subject/subject-screen.component';
+import { SelectSubjectsComponent } from './select-subjects/selectsubjects-screen.component';
 
 const APP_ROUTES: Routes = [
   { path: '', component: SigninScreenComponent, pathMatch: 'full' },
@@ -10,6 +11,7 @@ const APP_ROUTES: Routes = [
   { path: 'materias-aprobadas', component: SubjectScreenComponent },
   { path: 'verificacion-de-datos', component: DataVerificationComponent },
   { path: '**', redirectTo: '' }
+  { path: 'seleccionar-materias', component: SelectSubjectsComponent },
 ];
 
 export const Routing = RouterModule.forRoot(APP_ROUTES);
