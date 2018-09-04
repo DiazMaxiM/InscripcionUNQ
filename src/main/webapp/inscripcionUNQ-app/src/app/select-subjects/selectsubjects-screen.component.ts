62fc477791d92f6cd66b93a14e7eb0c9b87ce329
Ingrid Calderon <ingridgcalderon@gmail.com>
2018-09-04 19:11:13 -0300

    Modification of checkbox to open a modal when selected

--- src/main/webapp/inscripcionUNQ-app/src/app/select-subjects/selectsubjects-screen.component.ts
+++ src/main/webapp/inscripcionUNQ-app/src/app/select-subjects/selectsubjects-screen.component.ts
@@ -1,4 +1,10 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig} from '@angular/material';
+import {CustomDialogSubjectComponent} from '../custom-dialog-subject/custom-dialog-subject.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RestService } from '../rest.service';
+import { PollService } from '../poll/poll.service';
+import { PollInfo } from '../poll/poll-info.model';
 
 @Component({
   selector: 'app-selectsubjects-screen',
@@ -6,6 +12,49 @@ import { Component} from '@angular/core';
   styleUrls: ['./selectsubjects-screen.component.css']
 })
 
-export class SelectSubjectsComponent {
+export class SelectSubjectsComponent implements OnInit{
 
+  constructor(
+    private restService: RestService,
+    private pollService: PollService,
+    private formBuilder: FormBuilder,
+    private dialog: MatDialog
+  ) {}
+
+  pollInfo: PollInfo;
+  dataVerificationForm: FormGroup;
+  subjects: any;
+  checked:boolean;
+
+  ngOnInit() {
+    this.pollService.currentPollInfo.subscribe((pollInfo: PollInfo) => {
+        this.pollInfo = pollInfo;
+        this.getSubjets();
+      });
+    }
+
+  aceptar() {
+    if(this.checked){
+      this.openDialog('');
+    }
+  }
+
+  openDialog(msg: String) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = {
+        message: msg
+      };
+    const dialogRef = this.dialog.open(CustomDialogSubjectComponent,
+      dialogConfig);
+    }
+
+    //Modificar y quedarme con las materias no aprobadas
+  getSubjets() {
+    this.restService.getSubjets(this.pollInfo.idStudent)
+    .subscribe(subjects => {
+      this.subjects = subjects;
+    });
+  }
 }
