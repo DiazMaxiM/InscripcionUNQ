daba7be096f2aa54cc6373c784b138c07a91b415
Ingrid Calderon <ingridgcalderon@gmail.com>
2018-09-06 19:01:38 -0300

    Add dialog for the entry of invalid dni

--- src/main/webapp/inscripcionUNQ-app/src/app/auth/signin-screen.component.ts
+++ src/main/webapp/inscripcionUNQ-app/src/app/auth/signin-screen.component.ts
@@ -4,6 +4,9 @@ import { RestService } from '../rest.service';
 import { PollService } from '../poll/poll.service';
 import { Router } from '@angular/router';
 import { PollInfo } from '../poll/poll-info.model';
+import { CustomDialogComponent } from '../custom-dialog/custom-dialog.component';
+import { MatDialog, MatDialogConfig} from '@angular/material';
+import {HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-signin-screen',
@@ -15,21 +18,36 @@ export class SigninScreenComponent {
   constructor(
     private restService: RestService,
     private router: Router,
-    private pollService: PollService
+    private pollService: PollService,
+    private dialog: MatDialog
   ) {}
 
   dniFormControl = new FormControl('', [
       Validators.required
     ]);
   onSubmit() {
-    if (this.dniFormControl.valid) {
       const dni = this.dniFormControl.value;
-      this.restService.getPolls(this.dniFormControl.value).subscribe(polls => {
-      const pollInfo = new PollInfo(dni,polls);
-      this.pollService.sendStudentPollInfo(pollInfo);
+
+      this.restService.getPolls(dni).subscribe(polls => {
+        const pollInfo = new PollInfo(dni,polls);
+        this.pollService.sendStudentPollInfo(pollInfo);
+        this.router.navigate(['encuestas']);
       },
-          err => {console.log('error')});
-    }
-      this.router.navigate(['encuestas']);
+      (err:HttpErrorResponse) => {
+          this.router.navigate(['home']);
+          //Acá hay que llamar al servicio que me devuelve el mensaje de error
+          this.openDialog('ACÁ VA EL MENSAJE QUE ME VIENE DEL JSON');
+      });
+  }
+
+  openDialog(msg: String) {
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.disableClose = true;
+      dialogConfig.autoFocus = true;
+      dialogConfig.data = {
+          message: msg
+        };
+      const dialogRef = this.dialog.open(CustomDialogComponent,
+        dialogConfig);
   }
 }
