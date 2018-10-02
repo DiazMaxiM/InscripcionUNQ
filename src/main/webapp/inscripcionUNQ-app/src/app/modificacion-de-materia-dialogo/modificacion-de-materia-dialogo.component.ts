749640fccd63167af54f87d249a8faccbfdbf128
Ingrid Calderon <ingridgcalderon@gmail.com>
2018-10-02 06:41:14 -0300

    Actualización de datos de materia

--- src/main/webapp/inscripcionUNQ-app/src/app/modificacion-de-materia-dialogo/modificacion-de-materia-dialogo.component.ts
+++ src/main/webapp/inscripcionUNQ-app/src/app/modificacion-de-materia-dialogo/modificacion-de-materia-dialogo.component.ts
@@ -91,7 +91,9 @@ export class ModificacionDeMateriaDialogoComponent implements OnInit {
                 'nombre': '',
                 'horas': ''
             });
+            this.checked = this.materia.estado;
         }
+
     }
 
     guardar() {
@@ -105,4 +107,75 @@ export class ModificacionDeMateriaDialogoComponent implements OnInit {
     cerrar() {
         this.dialogRef.close();
     }
+    onChange(carrera) {
+
+    
+  
+    }
 }
+/*
+import {Compoent, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
+import { Carrera } from '../carreras/carrera.model';
+import { DataDialogo } from './data-dialogo.model';
+import { UtilesService } from '../utiles.service';
+
+@Component({
+    selector: 'app-carrera-dialogo',
+    templateUrl: './carrera-dialogo.component.html',
+    styleUrls: ['../dialogo-abm.component.css']
+})
+export class CarreraDialogoComponent implements OnInit {
+
+    form: FormGroup;
+    carrera: Carrera;
+    checked = false;
+
+    constructor(
+        private fb: FormBuilder,
+        private utilesService: UtilesService,
+        private dialogRef: MatDialogRef<CarreraDialogoComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: DataDialogo ) {
+            this.carrera = data.carrera;
+    }
+    ngOnInit() {
+        this.crearFormularioCarrera();
+        this.insertarInformacionDeLaCarreraEnFormulario();
+
+    }
+
+    crearFormularioCarrera() {
+        this.form = this.fb.group({
+            codigo: ['', Validators.required],
+            descripcion: ['', Validators.required]
+        });
+    }
+
+    insertarInformacionDeLaCarreraEnFormulario() {
+        if (this.carrera != null) {
+
+            this.form.setValue({
+                'codigo': this.carrera.codigo,
+                'descripcion': this.carrera.descripcion,
+              });
+            this.checked = this.carrera.habilitada;
+        }
+    }
+
+
+    guardar() {
+        if (this.form.valid) {
+            const { codigo, descripcion} = this.form.value;
+            const carrera = new Carrera(codigo, descripcion, this.checked);
+            this.dialogRef.close(carrera);
+        }
+    }
+
+    cerrar() {
+        this.dialogRef.close();
+    }
+
+}
+
+ */
