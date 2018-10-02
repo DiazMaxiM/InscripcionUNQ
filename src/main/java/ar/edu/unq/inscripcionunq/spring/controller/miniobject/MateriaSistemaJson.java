b08afa732a958d9dda778a4696530e0a2cbb929d
Ingrid Calderon <ingridgcalderon@gmail.com>
2018-10-02 06:41:14 -0300

    Actualización de datos de materia

---
+++ src/main/java/ar/edu/unq/inscripcionunq/spring/controller/miniobject/MateriaSistemaJson.java
@@ -0,0 +1,51 @@
+package ar.edu.unq.inscripcionunq.spring.controller.miniobject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unq.inscripcionunq.spring.model.Carrera;
+import ar.edu.unq.inscripcionunq.spring.model.Comision;
+import ar.edu.unq.inscripcionunq.spring.model.Materia;
+
+public class MateriaSistemaJson {
+	public Long id;
+	public String codigo;
+	public String nombre;
+    public Integer horas;
+	public boolean estado;
+//	public List<Carrera> carreras;
+    public List<CarreraJson> carreras;
+
+	public MateriaSistemaJson() {
+
+	}
+
+	public MateriaSistemaJson(Materia materia, boolean estado) {
+		this.id = materia.getId();
+        this.codigo = materia.getCodigo();
+		this.nombre = materia.getNombre();
+		this.horas = materia.getHoras();
+//		this.carreras = materia.getCarreras();
+        this.estado = estado;
+	}
+
+ /*   public MateriaSistemaJson(Long id, String codigo, String nombre, Integer horas, List<Carrera> carreras, boolean estado) {
+		this.id = id;
+        this.codigo = codigo;
+		this.nombre = nombre;
+		this.horas = horas;
+		this.carreras = carreras;
+        this.estado = estado;
+	}*/
+
+     public MateriaSistemaJson(Long id, String codigo, String nombre, Integer horas, List<CarreraJson> carreras, boolean estado) {
+		this.id = id;
+        this.codigo = codigo;
+		this.nombre = nombre;
+		this.horas = horas;
+		this.carreras = carreras;
+        this.estado = estado;
+	}
+
+	
+}
