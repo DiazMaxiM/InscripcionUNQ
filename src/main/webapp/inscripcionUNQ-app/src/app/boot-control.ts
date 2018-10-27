41e97a5d158c8d15891773ca100b9cee846746bc
rozaracho <zaracho.rosali@gmail.com>
2018-10-27 17:48:55 -0300

    Configuración para resetear la aplicación al salir

---
+++ src/main/webapp/inscripcionUNQ-app/src/app/boot-control.ts
@@ -0,0 +1,21 @@
+import { Subject } from 'rxjs/Subject';
+export class BootController {
+  private static instance: BootController;
+  private _reboot: Subject<boolean> = new Subject();
+  private reboot$ = this._reboot.asObservable();
+
+  static getbootControl() {
+    if (!BootController.instance) {
+      BootController.instance = new BootController();
+    }
+    return BootController.instance;
+  }
+
+  public watchReboot() {
+    return this.reboot$;
+  }
+
+  public restart() {
+    this._reboot.next(true);
+  }
+}
--- src/main/webapp/inscripcionUNQ-app/src/app/material.module.ts
+++ src/main/webapp/inscripcionUNQ-app/src/app/material.module.ts
@@ -14,6 +14,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatTooltipModule} from '@angular/material';
+import {MatMenuModule} from '@angular/material/menu';
 
 const modules = [
   MatInputModule,
@@ -31,7 +32,8 @@ const modules = [
   MatProgressSpinnerModule,
   MatSlideToggleModule,
   MatAutocompleteModule,
-  MatTooltipModule
+  MatTooltipModule,
+  MatMenuModule
 ];
 
 @NgModule({
