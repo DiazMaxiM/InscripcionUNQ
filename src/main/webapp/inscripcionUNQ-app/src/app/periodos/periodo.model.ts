8cfa4fadf0d99604696ecd84b7519f4709c936f9
Ingrid Calderon <ingridgcalderon@gmail.com>
2018-10-27 22:50:29 -0300

    Validación para no perder datos de período y mensaje de creación exitosa

--- src/main/webapp/inscripcionUNQ-app/src/app/periodos/periodo.model.ts
+++ src/main/webapp/inscripcionUNQ-app/src/app/periodos/periodo.model.ts
@@ -1,18 +1,18 @@
 export class Periodo {
-    id?: string;
-    codigo?: string;
-    anho?: number;
-    numero?: number;
-    tipoPeriodo?: string;
+	id?: string;
+	codigo?: string;
+	anho?: number;
+	numero?: number;
+	tipoPeriodo?: string;
 
-  constructor(
-    anho?: number,
-    numero?: number,
-    tipoPeriodo?: string,
+	constructor(
+		anho?: number,
+		numero?: number,
+		tipoPeriodo?: string,
 
-  ) {
-    this.anho = anho;
-    this.numero = numero;
-    this.tipoPeriodo = tipoPeriodo;
-  }
+	) {
+		this.anho = anho;
+		this.numero = numero;
+		this.tipoPeriodo = tipoPeriodo;
+	}
 }
